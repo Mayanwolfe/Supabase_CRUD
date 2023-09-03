714ff852626934e5ce31d5cb4fda54da0291a562
Claire Bourdon <cebourdon625@gmail.com>
2023-09-03 12:06:15 -0400

    small change to format

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = process.env.REACT_APP_SUPABASE_API_KEY;
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_API_KEY
+);
+
+export { supabase };
